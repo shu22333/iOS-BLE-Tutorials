a3a1f57bf033bf5941f5ce88696383c1fa28c146
jiro33 <nagasaki@sansan.com>
2024-07-18 12:29:49 +0900

    feat: アドバタイズも実装

--- 4-1_Scan/BLEScanExample/BLEScanExample/ViewController.swift
+++ 4-1_Scan/BLEScanExample/BLEScanExample/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController {
+    @IBOutlet var advertiseBtn: UIButton!
 
     var isScanning = false
     var centralManager: CBCentralManager!
-    
+    private var peripheralManager: CBPeripheralManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // ペリフェラルマネージャ初期化
+        peripheralManager = CBPeripheralManager(
+            delegate: self,
+            queue: nil,
+            options: nil)
+
         // セントラルマネージャ初期化
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
@@ -38,6 +46,14 @@ class ViewController: UIViewController {
             isScanning = false
         }
     }
+
+    @IBAction func advertiseButtonTapped(_ sender: UIButton) {
+        if !peripheralManager.isAdvertising {
+            startAdvertise()
+        } else {
+            stopAdvertise()
+        }
+    }
 }
 
 extension ViewController: CBCentralManagerDelegate {
@@ -57,3 +73,45 @@ extension ViewController: CBCentralManagerDelegate {
     }
 }
 
+extension ViewController: CBPeripheralManagerDelegate {
+
+    // ペリフェラルマネージャの状態が変化すると呼ばれる
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        print("state: \(peripheral.state)")
+
+        switch peripheral.state {
+        case .poweredOn:
+            // アドバタイズ開始
+            startAdvertise()
+        default:
+            break
+        }
+    }
+
+    // アドバタイズ開始処理が完了すると呼ばれる
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if let error = error {
+            print("アドバタイズ開始失敗！ error: \(error)")
+            return
+        }
+        print("アドバタイズ開始成功！")
+    }
+
+    private func startAdvertise() {
+        // アドバタイズメントデータを作成する
+        let serviceUUID = CBUUID(string: "B36F4066-2EF7-467E-832D-8CBFF563BBB7")
+        let advertisementData = [CBAdvertisementDataServiceUUIDsKey: [serviceUUID]]
+
+        // アドバタイズ開始
+        peripheralManager.startAdvertising(advertisementData)
+
+        advertiseBtn.setTitle("STOP ADVERTISING", for: .normal)
+    }
+
+    private func stopAdvertise () {
+        // アドバタイズ停止
+        peripheralManager.stopAdvertising()
+
+        advertiseBtn.setTitle("START ADVERTISING", for: .normal)
+    }
+}
